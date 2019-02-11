69310e3197972bb1a676a105c109477efdea8c80
ncr0zier <36426670+ncr0zier@users.noreply.github.com>
2019-02-10 20:08:18 -0500

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -5,8 +5,11 @@
 -- get information for all customers.
 SELECT * FROM customer;
 
--- get all information for all orders.
-SELECT * FROM order;
+-- get all information for all transactions
+SELECT * FROM tranaction;
+
+-- get all information for all shipments
+SELECT * FROM shipment;
 
 -- get all information for all products.
 SELECT * FROM product;
@@ -20,8 +23,11 @@ SELECT * FROM category;
 -- get information for a customer by id.
 SELECT * FROM customer WHERE id = :customer_id_selected_from_page;
 
--- get information for a order by id.
-SELECT * FROM order WHERE id = :order_id_selected_from_page;
+-- get information for a tranaction by id.
+SELECT * FROM tranaction WHERE id = :transaction_id_selected_from_page;
+
+-- get information for a shipment by id.
+SELECT * FROM shipment WHERE id = :shipment_id_selected_from_page;
 
 -- get information for a product by id.
 SELECT * FROM product WHERE id = :product_id_selected_from_page;
@@ -31,3 +37,21 @@ SELECT * FROM inventory_item WHERE id = :item_id_selected_from_page;
 
 -- get information for a category by id.
 SELECT * FROM category WHERE id = :category_id_selected_from_page;
+
+-- add a new customer
+INSERT INTO customer (fname, lname, phonenumber) VALUES (:fnameInput, :lnameInput, :phoneInput);
+
+-- add a new transaction
+INSERT INTO transaction (customer, `date`, payment_method, payment_total) VALUES (:customerInput, :dateInput, :methodInput, :totalInput);
+
+-- add a new shipment
+INSERT INTO shipment (supplier_name, shipping_service_name, `date`, damaged) VALUES (:supplierInput, :serviceInput, :dateInput, :damagedInput);
+
+-- add a new product
+INSERT INTO shipment (name, selling_price) VALUES (:nameInput, :priceInput);
+
+-- add a new item
+INSERT INTO shipment (pid, serial, buying_price, shipmentID, transactionID) VALUES (:pidInput, :serialInput, :priceInput, :shipmentInput, :transactionInput);
+
+-- add a new category for an item.
+INSERT INTO shipment (name) VALUES (:nameInput);
