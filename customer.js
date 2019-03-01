4c213ca7197b093b0a771095649eb53fbbe54916
tonyenike <martinb3@kec1130-21.engr.oregonstate.edu>
2019-02-28 19:45:56 -0800

    Added some more get functions for customer

--- customer.js
+++ customer.js
@@ -8,26 +8,40 @@ module.exports = function(){
     var YEET  = require('./engine.js');
     var YOTE = new YEET();
 
-    function getCategories(res, mysql, context){
+    function getCategories(res, mysql, context, complete){
         var q = YOTE.queryText6;
-        console.log(q);
         mysql.pool.query(q, function(error, results, fields){
                 if(error){
                 res.write(JSON.stringify(error));
                 res.end();
                 }
                 context.cat = results;
+                complete();
                 });
     }
 
+    function getProducts(res, mysql, context, complete){
+        var q = YOTE.queryText6;
+        mysql.pool.query(q, function(error, results, fields){
+                if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+                }
+                context.products = results;
+                complete();
+                });
+    }
 
 
 
     router.get('/', function(req, res){
             var context = {};
             var mysql = req.app.get('mysql');
-            getCategories(res, mysql, context);
-            res.render('customer', context);
+            getCategories(res, mysql, context, complete);
+            getProducts(res, mysql, context, complete);
+            function complete(){
+                res.render('customer', context);
+            }
             });
 
     return router;
