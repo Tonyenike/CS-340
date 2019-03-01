2d14816d4668f106c64d01abe223b739fdaf6ae7
ncr0zier <36426670+ncr0zier@users.noreply.github.com>
2019-02-28 21:53:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', express.static('public'));
 app.use('/customer', require('./customer.js'));
 app.use('/employee', require('./employee.js'));
+app.use('/create', require('./create.js'));
 
 var url = require('url');
 
