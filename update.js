50f9afa9538ab6fdd14faf9d2de7cf5744372805
ncr0zier <36426670+ncr0zier@users.noreply.github.com>
2019-03-10 22:51:00 -0400

    Update update.js

--- update.js
+++ update.js
@@ -6,8 +6,10 @@ module.exports = function() {
   queryUpdateCustomer = "UPDATE customer SET fname = ?, lname = ?, phone_number = ? WHERE id = ?";
 
   router.put('/updateCustomer', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.fnameInput, req.body.lnameInput, req.body.phoneInput, req.body.idCustUpdate];
+
     sql = mysql.pool.query(queryUpdateCustomer, inserts, function(error, results, fields) {
       if (error) {
         res.write(JSON.stringify(error));
@@ -23,6 +25,7 @@ module.exports = function() {
   queryUpdateTransaction = "UPDATE transaction SET customer = ?, date = ?, payment_method = ?, payment_total = ? WHERE id = ?";
 
   router.put('/updateTransaction', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.customerInput, req.body.dateInput, req.body.methodInput, req.body.totalInput, req.body.idTransUpdate];
     sql = mysql.pool.query(queryUpdateTransaction, inserts, function(error, results, fields) {
@@ -40,6 +43,7 @@ module.exports = function() {
   queryUpdateShipment = "UPDATE shipment SET supplier_name = ?, shipping_service_name = ?, date = ?, damaged = ? WHERE id = ?";
 
   router.put('/updateShipment', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.supplierInput, req.body.serviceInput, req.body.dateInput, req.body.damagedInput, req.body.idShipUpdate];
     sql = mysql.pool.query(queryUpdateShipment, inserts, function(error, results, fields) {
@@ -57,6 +61,7 @@ module.exports = function() {
   queryUpdateProduct = "UPDATE product SET name = ?, selling_price = ? WHERE id = ?";
 
   router.put('/updateProduct', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.nameInput, req.body.priceInput, req.body.idProdUpdate];
     sql = mysql.pool.query(queryUpdateProduct, inserts, function(error, results, fields) {
@@ -74,6 +79,7 @@ module.exports = function() {
   queryUpdateItem = "UPDATE inventory_item SET pid = ?, serial = ?, buying_price = ?, shipmentID = ?, transactionID = ? WHERE id = ?";
 
   router.put('/updateItem', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.pidInput, req.body.serialInput, req.body.priceInput, req.body.shipmentInput, req.body.transactionInput, req.body.idItemUpdate];
     sql = mysql.pool.query(queryUpdateItem, inserts, function(error, results, fields) {
@@ -91,6 +97,7 @@ module.exports = function() {
   queryUpdateCategory = "UPDATE category SET name = ? WHERE id = ?";
 
   router.put('/updateCategory', function(req, res) {
+    console.log("PUT CALLED");
     var mysql = req.app.get('mysql');
     var inserts = [req.body.nameInput, req.body.idCatUpdate];
     sql = mysql.pool.query(queryUpdateCategory, inserts, function(error, results, fields) {
