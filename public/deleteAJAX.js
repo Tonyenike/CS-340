8cbdccf76ba478514daf8a706d768370534f1fbd
ncr0zier <36426670+ncr0zier@users.noreply.github.com>
2019-03-10 22:51:22 -0400

    Update deleteAJAX.js

--- public/deleteAJAX.js
+++ public/deleteAJAX.js
@@ -1,4 +1,5 @@
 function deleteItem(id){
+  console.log("DELETE CALLED");
   $.ajax({
       url: '/deleteItem/' + id,
       type: 'DELETE',
@@ -9,41 +10,57 @@ function deleteItem(id){
 };
 
 function deleteProduct(id){
+  console.log("DELETE CALLED");
   $.ajax({
       url: '/deleteProduct/' + id,
-      type: 'DELETE',
-      success: function(result){
-          window.location.reload(true);
-      }
-  })
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
 };
 
 function deleteTransaction(id){
-  $.ajax({
-      url: '/deleteTransaction/' + id,
-      type: 'DELETE',
-      success: function(result){
-          window.location.reload(true);
-      }
-  })
+  console.log("DELETE CALLED");
+    $.ajax({
+        url: '/deleteTransaction/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
 };
 
 function deleteShipment(id){
-  $.ajax({
-      url: '/deleteShipment/' + id,
-      type: 'DELETE',
-      success: function(result){
-          window.location.reload(true);
-      }
-  })
+  console.log("DELETE CALLED");
+    $.ajax({
+        url: '/deleteShipment/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
 };
 
 function deleteCategory(id){
-  $.ajax({
-      url: '/deleteCategory/' + id,
-      type: 'DELETE',
-      success: function(result){
-          window.location.reload(true);
-      }
-  })
+  console.log("DELETE CALLED");
+    $.ajax({
+        url: '/deleteCategory/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
+
+function deleteCustomer(id){
+  console.log("DELETE CALLED");
+  console.log(id);
+    $.ajax({
+        url: '/deleteCustomer/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
 };
