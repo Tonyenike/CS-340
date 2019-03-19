851368a86661eed800f1a62145726d755bda2bc0
tonyenike <martinb3@flip1.engr.oregonstate.edu>
2019-03-19 13:23:25 -0700

    Added error checking

--- public/shipper.js
+++ public/shipper.js
@@ -1,23 +1,53 @@
 function addNewShipment(){
 
-    alert("This function doesn't do anything yet!");
 
 
+    var rowcount = document.getElementById("shipment-items").rows.length;
 
+    var supplierName = document.getElementById('supplier-name').value;
+    var shipperName = document.getElementById('shipper-name').value;
+    if(supplierName === ""){
+        alert("Please provide a supplier name.");
+        return;
+    }
+    else if(shipperName === ""){
+        alert("Please provide a shipper name.");
+        return;
+    }
 
+    var prices = document.getElementsByClassName('product-price');
+    var serials = document.getElementsByClassName('product-serial');
+    var products = document.getElementsByClassName('product-name');
 
+    var i
+    for(i = 0; i < prices.length; i++){
+       
+        console.log(products[i].value); 
+        if (parseFloat(prices[i].value) > 0){} 
+        else{
+            alert("Wholesale amount is invalid");
+            return;
+        }
+        if(parseFloat(+prices[i].value) == parseFloat(+prices[i].value).toFixed(2)){}
+        else{
+            alert("Wholesale amount is in a fraction of a cent!");
+            return;
+        }
+        if (Number.isInteger(+serials[i].value)){
+            //data is an integer
+        }
+        else{
+            alert("Serial number is not an integer");
+            return;
+        }
+        if(parseInt(+serials[i].value) > 0){}
+        else{
+            alert("Serial number is invalid -- it must be greater than 0");
+            return;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    alert("This button doesn't work yet!")
 
 
 }
